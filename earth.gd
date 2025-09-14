d2ebb1d691cdff85721fa1b8b24a8ced97d8e848
Matheus Sabino <matheusouzatj@gmail.com>
2025-09-14 19:25:42 -0300

    fix earth damage flash

--- earth.gd
+++ earth.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+
 func _ready():
 	var screen_size = get_viewport_rect().size
 	position = (screen_size) / 2
@@ -7,11 +8,10 @@ func _ready():
 func _process(delta: float) -> void:
 	rotation += 0.03*delta
 
+func _on_area_entered(area: Area2D) -> void:
+	flash_damage()
+
 func flash_damage():
 	var tween = create_tween()
 	tween.tween_property($Sprite2D, "modulate", Color(1, 0, 0), 0.1)
 	tween.tween_property($Sprite2D, "modulate", Color(1, 1, 1), 0.1)
-
-func _on_area_entered(area: Area2D):
-	flash_damage()
-	pass # Replace with function body.
